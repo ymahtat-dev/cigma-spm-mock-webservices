ed09242133033423ed358f71b3f49405942d619a
Youssef MAHTAT <youssef.mahtat.external@atos.net>
2024-06-06 18:22:22 +0100

    upload project

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# cigma-spm-mock-webservices
+# cigma-spm-mock-webservices (SPM Mock Webservices)
+
+
+## Description du Projet :
+Ce projet mock est conçu pour les étudiants du laboratoire Angular afin de leur fournir une API backend simulée en utilisant `json-server`. L'objectif est de permettre aux étudiants de se familiariser avec les interactions backend/frontend dans une application de gestion de projets scolaires.
+
+## Fonctionnement du Projet :
+Le projet utilise `json-server` pour simuler une API RESTful. Les données sont stockées dans un fichier JSON (`db.json`) et les routes API sont définies dans `server.js`.
+
+### Clonez le Projet :
+Pour cloner le projet, utilisez la commande suivante :
+```
+git clone https://github.com/ymahtat-dev/cigma-spm-mock-webservices.git
+cd cigma-spm-mock-webservices
+```
+
+### Installation des Dépendances :
+Pour installer les dépendances, utilisez la commande suivante :
+```
+npm install
+```
+
+### Démarrage du Serveur :
+Pour démarrer le serveur, utilisez la commande suivante :
+```
+npm start
+```
+ou
+```
+node server.js
+```
+Le serveur s'exécutera sur http://localhost:3000.
+Tous les services ont l'url prefixé par http://localhost:3000/api.
+
+### Authentification :
+L'authentification est gérée à l'aide de JSON Web Tokens (JWT). Pour vous authentifier, envoyez une requête POST à http://localhost:3000/api/auth/login avec les informations d'identification suivantes :
+
+- Utilisateur Administrateur
+```
+{
+    "username": "admin",
+    "password": "admin"
+}
+```
+- Utilisateur Etudiant
+```
+{
+    "username": "azbennani",
+    "password": "1234567"
+}
+```
+
+En cas de succès, un token JWT sera retourné. Utilisez ce token pour accéder aux routes protégées en l'ajoutant dans l'en-tête Authorization de vos requêtes comme suit :
+```
+Authorization: Bearer <votre_token_jwt>
+```
+
+### Endpoints Disponibles (APIs) :
+- Authentification :
+  - POST http://localhost:3000/api/auth/login : Authentification de l'utilisateur.
+  - GET http://localhost:3000/api/auth/validate-token : Validation du token JWT.
+  - GET http://localhost:3000/api/auth/profile : Récupérer le profil de l'utilisateur connecté.
+  - PUT http://localhost:3000/api/auth/update-password : Modifier le mot de passe de l'utilisateur connecté.
+  - POST http://localhost:3000/api/auth/logout : Déconnexion de l'utilisateur.
+- Gestion des Utilisateurs (Admin uniquement) :
+  - GET http://localhost:3000/api/users : Récupérer tous les utilisateurs.
+  - POST http://localhost:3000/api/users : Créer un nouvel utilisateur.
+  - PUT http://localhost:3000/api/users/:id : Mettre à jour un utilisateur existant.
+  - DELETE http://localhost:3000/api/users/:id : Supprimer un utilisateur.
+- Gestion des Étudiants (Admin uniquement) :
+  - GET http://localhost:3000/api/students : Récupérer tous les étudiants.
+  - POST http://localhost:3000/api/students : Créer un nouvel étudiant.
+- Gestion des Projets :
+  - GET http://localhost:3000/api/projects : Récupérer tous les projets (Admin) ou les projets de l'utilisateur connecté.
+  - POST http://localhost:3000/api/projects : Créer un nouveau projet (Étudiant uniquement).
+  - PUT http://localhost:3000/api/projects/:id : Mettre à jour un projet (Admin ou Étudiant responsable).
+  - DELETE http://localhost:3000/api/projects/:id : Supprimer un projet (Admin ou Étudiant responsable).
+  - GET http://localhost:3000/api/projects/:id : Récupérer un projet spécifique (Admin, Étudiant responsable ou membre).
+- Gestion des Tâches :
+  - GET http://localhost:3000/api/tasks : Récupérer toutes les tâches (Admin) ou les tâches de l'utilisateur connecté.
+  - GET http://localhost:3000/api/projects/:projectId/tasks : Récupérer les tâches d'un projet spécifique (Admin, Étudiant responsable ou membre).
+  - POST http://localhost:3000/api/projects/:projectId/tasks : Créer une nouvelle tâche dans un projet (Admin, Étudiant responsable ou membre).
+  - PUT http://localhost:3000/api/tasks/:id : Mettre à jour une tâche (Admin ou propriétaire de la tâche).
+  - DELETE http://localhost:3000/api/tasks/:id : Supprimer une tâche (Admin ou propriétaire de la tâche).
+  - GET http://localhost:3000/api/tasks/:id : Récupérer une tâche spécifique (Admin ou propriétaire de la tâche).
+- Gestion des Commentaires :
+  - GET http://localhost:3000/api/comments : Récupérer tous les commentaires (Admin) ou les commentaires de l'utilisateur connecté.
+  - GET http://localhost:3000/api/tasks/:taskId/comments : Récupérer les commentaires d'une tâche spécifique (Admin ou propriétaire de la tâche).
+  - POST http://localhost:3000/api/tasks/:taskId/comments : Créer un nouveau commentaire dans une tâche (Admin ou propriétaire de la tâche).
+  - PUT http://localhost:3000/api/comments/:id : Mettre à jour un commentaire (Admin ou propriétaire du commentaire).
+  - DELETE http://localhost:3000/api/comments/:id : Supprimer un commentaire (Admin ou propriétaire du commentaire).
+  - GET http://localhost:3000/api/comments/:id : Récupérer un commentaire spécifique (Admin ou propriétaire du commentaire).
+- Données Maîtres (sans authentification) :
+  - GET http://localhost:3000/api/masterdata/user-types : Récupérer les types d'utilisateurs.
+  - GET http://localhost:3000/api/masterdata/studies-types : Récupérer les types de programmes d'études.
+  - GET http://localhost:3000/api/masterdata/status-flows : Récupérer les statuts de flux.
+  - GET http://localhost:3000/api/masterdata/task-types : Récupérer les types de tâches
---
+++ server.js
@@ -0,0 +1,431 @@
+const jsonServer = require('json-server');
+const jwt = require('jsonwebtoken');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const bodyParser = require('body-parser');
+
+const SECRET_KEY = 'cigma-test-secret';
+const expiresIn = '1h';
+
+server.use(middlewares);
+server.use(bodyParser.json());
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", null); // Suppression de l'en-tête CORS
+    //res.header("Access-Control-Allow-Origin", "*"); // allow all origins de l'en-tête CORS
+    next();
+});
+
+
+// Endpoints pour récupérer les données maîtres (sans auth)
+server.get('/api/masterdata/user-types', (req, res) => {
+    const userTypes = router.db.get('masterdata.user-types').value();
+    res.json(userTypes);
+});
+
+server.get('/api/masterdata/studies-types', (req, res) => {
+    const studiesTypes = router.db.get('masterdata.studies-types').value();
+    res.json(studiesTypes);
+});
+
+server.get('/api/masterdata/status-flows', (req, res) => {
+    const statusFlows = router.db.get('masterdata.status-flows').value();
+    res.json(statusFlows);
+});
+
+server.get('/api/masterdata/task-types', (req, res) => {
+    const taskTypes = router.db.get('masterdata.task-types').value();
+    res.json(taskTypes);
+});
+
+// Middleware d'authentification
+server.post('/api/auth/login', (req, res) => {
+    const { username, password } = req.body;
+    const users = router.db.get('users').value();
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const token = jwt.sign({ id: user.id, username: user.username, userType: user.userType }, SECRET_KEY, { expiresIn });
+        res.status(200).json({
+            message: 'Login successful',
+            token
+        });
+    } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+    }
+});
+
+// Middleware pour valider le token
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, SECRET_KEY, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+// Route pour valider le token
+server.get('/api/auth/validate-token', authenticateJWT, (req, res) => {
+    res.status(200).json({
+        message: 'Token is valid',
+        user: req.user
+    });
+});
+
+// Route pour récupérer le profil de l'utilisateur
+server.get('/api/auth/profile', authenticateJWT, (req, res) => {
+    const user = router.db.get('users').find({ id: req.user.id }).value();
+    if (user) {
+        delete user.password;
+        res.json(user);
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+});
+
+// Route pour modifier son mot de passe
+server.put('/api/auth/update-password', authenticateJWT, (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+    const user = router.db.get('users').find({ id: req.user.id }).value();
+    if (user && user.password === oldPassword) {
+        router.db.get('users').find({ id: req.user.id }).assign({ password: newPassword }).write();
+        res.status(200).json({ message: 'Password updated successfully' });
+    } else {
+        res.status(400).json({ message: 'Incorrect old password' });
+    }
+});
+
+// Route pour déconnexion
+server.post('/api/auth/logout', authenticateJWT, (req, res) => {
+    // Invalidate token logic can be implemented here if needed
+    res.status(200).json({ message: 'Logout successful' });
+});
+
+// Gestion des utilisateurs (Admin seulement)
+server.use('/api/users', authenticateJWT, (req, res, next) => {
+    if (req.user.userType !== 'admin') {
+        res.sendStatus(403);
+    } else {
+        next();
+    }
+});
+
+server.get('/api/users', (req, res) => {
+    const users = router.db.get('users').value();
+    const recoveredUsers = JSON.parse(JSON.stringify(users));
+    if (recoveredUsers) {
+        recoveredUsers.forEach(user => {
+            delete user.password;
+        });
+    }
+    res.json(recoveredUsers);
+});
+
+server.post('/api/users', authenticateJWT, (req, res) => {
+    if (req.user.userType !== 'admin') {
+        return res.sendStatus(403);
+    }
+    const userPayload = req.body;
+    const userToCreate = {
+        id: router.db.get('users').value().length + 1,
+        firstname: userPayload.firstname,
+        lastname: userPayload.lastname,
+        username: userPayload.username,
+        password: userPayload.password,
+        gender: userPayload.gender,
+        birthdate: userPayload.birthdate,
+        email: userPayload.email,
+        picture: userPayload.picture,
+        userType: userPayload.userType,
+        studentId: userPayload.studentId
+    };
+    router.db.get('users').push(userToCreate).write();
+    const createdUser = JSON.parse(JSON.stringify(userToCreate));
+    delete createdUser.password;
+    res.status(201).json(createdUser);
+});
+
+server.put('/api/users/:id', authenticateJWT, (req, res) => {
+    if (req.user.userType !== 'admin') {
+        return res.sendStatus(403);
+    }
+    const { id } = req.params;
+    const userPayload = req.body;
+    router.db.get('users').find({ id: parseInt(id) }).assign(userPayload).write();
+    const updatedUser = JSON.parse(JSON.stringify(userPayload));
+    delete updatedUser.password;
+    res.status(200).json(updatedUser);
+});
+
+server.delete('/api/users/:id', authenticateJWT, (req, res) => {
+    if (req.user.userType !== 'admin') {
+        return res.sendStatus(403);
+    }
+    const { id } = req.params;
+    router.db.get('users').remove({ id: parseInt(id) }).write();
+    res.sendStatus(204);
+});
+
+// Gestion des étudiants
+server.use('/api/students', authenticateJWT, (req, res, next) => {
+    if (!req.user.userType) {
+        res.sendStatus(400);
+    } else {
+        next();
+    }
+});
+
+server.get('/api/students', (req, res) => {
+    const users = router.db.get('users').value().filter(u => u.userType === 'student');
+    const recoveredStudents = JSON.parse(JSON.stringify(users));
+    if (recoveredStudents) {
+        recoveredStudents.forEach(student => {
+            delete student.password;
+        });
+    }
+    res.json(recoveredStudents);
+});
+
+server.post('/api/students', authenticateJWT, (req, res) => {
+    if (req.user.userType !== 'admin') {
+        return res.sendStatus(403);
+    }
+    const studentPayload = req.body;
+    const studentToCreate = {
+        id: router.db.get('users').value().length + 1,
+        firstname: studentPayload.firstname,
+        lastname: studentPayload.lastname,
+        username: studentPayload.username,
+        password: studentPayload.password,
+        gender: studentPayload.gender,
+        birthdate: studentPayload.birthdate,
+        email: studentPayload.email,
+        picture: studentPayload.picture,
+        userType: 'student',
+        studentId: studentPayload.studentId,
+        study: studentPayload.study
+    };
+    router.db.get('users').push(studentToCreate).write();
+    const createdStudent = JSON.parse(JSON.stringify(studentToCreate));
+    delete createdStudent.password;
+    res.status(201).json(createdStudent);
+});
+
+// Gestion des projets
+server.get('/api/projects', authenticateJWT, (req, res) => {
+    const projects = req.user.userType === 'admin' ? router.db.get('projects').value() : router.db.get('projects').filter(p => p.studentsMembers.includes(req.user.id)).value();
+    res.json(projects);
+});
+
+server.post('/api/projects', authenticateJWT, (req, res) => {
+    if (req.user.userType !== 'student') {
+        return res.sendStatus(403);
+    }
+    const projectPayload = req.body;
+    const projectToCreate = {
+        id: router.db.get('projects').value().length + 1,
+        name: projectPayload.name,
+        description: projectPayload.description,
+        type: projectPayload.type,
+        status: projectPayload.status,
+        startDate: projectPayload.startDate,
+        provisionalEndDate: projectPayload.provisionalEndDate,
+        endDate: projectPayload.endDate,
+        responsibleStudentId: req.user.id,
+        studentsMembers: [req.user.id]
+    };
+    router.db.get('projects').push(projectToCreate).write();
+    res.status(201).json(projectToCreate);
+});
+
+server.put('/api/projects/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const project = router.db.get('projects').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || project.responsibleStudentId === req.user.id) {
+        router.db.get('projects').find({ id: parseInt(id) }).assign(req.body).write();
+        res.status(200).json(req.body);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.delete('/api/projects/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const project = router.db.get('projects').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || project.responsibleStudentId === req.user.id) {
+        router.db.get('projects').remove({ id: parseInt(id) }).write();
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.get('/api/projects/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const project = router.db.get('projects').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || project.responsibleStudentId === req.user.id || project.studentsMembers.includes(req.user.id)) {
+        res.json(project);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+// Gestion des tâches
+server.get('/api/tasks', authenticateJWT, (req, res) => {
+    const tasks = req.user.userType === 'admin' ? router.db.get('tasks').value() : router.db.get('tasks').filter(t => t.userId === req.user.id).value();
+    res.json(tasks);
+});
+
+server.get('/api/projects/:projectId/tasks', authenticateJWT, (req, res) => {
+    const { projectId } = req.params;
+    const project = router.db.get('projects').find({ id: parseInt(projectId) }).value();
+    if (req.user.userType === 'admin' || project.responsibleStudentId === req.user.id || project.studentsMembers.includes(req.user.id)) {
+        const tasks = router.db.get('tasks').filter(t => t.projectId === parseInt(projectId)).value();
+        res.json(tasks);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.post('/api/projects/:projectId/tasks', authenticateJWT, (req, res) => {
+    const { projectId } = req.params;
+    const project = router.db.get('projects').find({ id: parseInt(projectId) }).value();
+    if (req.user.userType === 'admin' || project.responsibleStudentId === req.user.id || project.studentsMembers.includes(req.user.id)) {
+        const taskPayload = req.body;
+        const taskToCreate = {
+            id: router.db.get('tasks').value().length + 1,
+            name: taskPayload.name,
+            description: taskPayload.description,
+            type: taskPayload.type,
+            status: taskPayload.status,
+            startDate: taskPayload.startDate,
+            provisionalEndDate: taskPayload.provisionalEndDate,
+            endDate: taskPayload.endDate,
+            projectId: parseInt(projectId),
+            userId: req.user.id
+        };
+        router.db.get('tasks').push(taskToCreate).write();
+        res.status(201).json(taskToCreate);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.put('/api/tasks/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const task = router.db.get('tasks').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || task.userId === req.user.id) {
+        const taskPayload = req.body;
+        router.db.get('tasks').find({ id: parseInt(id) }).assign(taskPayload).write();
+        res.status(200).json(taskPayload);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.delete('/api/tasks/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const task = router.db.get('tasks').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || task.userId === req.user.id) {
+        router.db.get('tasks').remove({ id: parseInt(id) }).write();
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.get('/api/tasks/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const task = router.db.get('tasks').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || task.userId === req.user.id) {
+        res.json(task);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+// Gestion des commentaires
+server.get('/api/comments', authenticateJWT, (req, res) => {
+    const comments = req.user.userType === 'admin' ? router.db.get('comments').value() : router.db.get('comments').filter(c => c.userId === req.user.id).value();
+    res.json(comments);
+});
+
+server.get('/api/tasks/:taskId/comments', authenticateJWT, (req, res) => {
+    const { taskId } = req.params;
+    const task = router.db.get('tasks').find({ id: parseInt(taskId) }).value();
+    if (req.user.userType === 'admin' || task.userId === req.user.id) {
+        const comments = router.db.get('comments').filter(c => c.taskId === parseInt(taskId)).value();
+        res.json(comments);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.post('/api/tasks/:taskId/comments', authenticateJWT, (req, res) => {
+    const { taskId } = req.params;
+    const task = router.db.get('tasks').find({ id: parseInt(taskId) }).value();
+    if (req.user.userType === 'admin' || task.userId === req.user.id) {
+        const commentPayload = req.body;
+        const commentToCreate = {
+            id: router.db.get('comments').value().length + 1,
+            text: commentPayload.text,
+            taskId: parseInt(taskId),
+            userId: req.user.id
+        };
+        router.db.get('comments').push(commentToCreate).write();
+        res.status(201).json(commentToCreate);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.put('/api/comments/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const comment = router.db.get('comments').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || comment.userId === req.user.id) {
+        const commentPayload = req.body;
+        router.db.get('comments').find({ id: parseInt(id) }).assign(commentPayload).write();
+        res.status(200).json(commentPayload);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.delete('/api/comments/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const comment = router.db.get('comments').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || comment.userId === req.user.id) {
+        router.db.get('comments').remove({ id: parseInt(id) }).write();
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+server.get('/api/comments/:id', authenticateJWT, (req, res) => {
+    const { id } = req.params;
+    const comment = router.db.get('comments').find({ id: parseInt(id) }).value();
+    if (req.user.userType === 'admin' || comment.userId === req.user.id) {
+        res.json(comment);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+
+
+// Utiliser le routeur par défaut avec le préfixe /api
+server.use('/api', router);
+
+
+server.listen(3000, () => {
+    console.log('JSON Server is running on port 3000');
+});
